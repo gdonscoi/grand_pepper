f4f70665118c492a18351ceeffc858f79eb759d6
Gerson Donscoi Jr <donscoijr@gmail.com>
2015-11-03 16:17:07 -0200

    beans for json

---
+++ app/src/main/java/net/grandpepper/caiena/grandpepper/beans/Event.java
@@ -0,0 +1,20 @@
+package net.grandpepper.caiena.grandpepper.beans;
+
+public class Event {
+
+    public int version;
+
+    public String eventType;
+
+    public String title;
+
+    public String subtitle;
+
+    public String date;
+
+    public String summary;
+
+    public String startTime;
+
+    public String endTime;
+}
---
+++ app/src/main/java/net/grandpepper/caiena/grandpepper/beans/Info.java
@@ -0,0 +1,22 @@
+package net.grandpepper.caiena.grandpepper.beans;
+
+public class Info {
+
+    public int version;
+
+    public String title;
+
+    public String subtitle;
+
+    public String date;
+
+    public String summary;
+
+    public String locationName;
+
+    public String backgroundImageUrl;
+
+    public Event events;
+
+    public String type;
+}
